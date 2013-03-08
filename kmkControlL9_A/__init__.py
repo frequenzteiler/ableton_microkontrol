48edd737dd5652dc72812af20942e8d0af2cb5de
Tamas Kalman <ktamas77@gmail.com>
2013-03-07 20:41:31 -0800

    Ableton 9 folders & clean up

---
+++ kmkControlL9_A/__init__.py
@@ -0,0 +1,11 @@
+# emacs-mode: -*- python-*-
+
+import Live
+from kmkControlA import kmkControlA
+
+def create_instance(c_instance):
+    """ Creates and returns the kmkControl-A script """
+    return kmkControlA(c_instance)
+
+# local variables:
+# tab-width: 4
---
+++ kmkControlL9_B/__init__.py
@@ -0,0 +1,11 @@
+# emacs-mode: -*- python-*-
+
+import Live
+from kmkControlB import kmkControlB
+
+def create_instance(c_instance):
+    """ Creates and returns the kmkControl-B script """
+    return kmkControlB(c_instance)
+
+# local variables:
+# tab-width: 4
